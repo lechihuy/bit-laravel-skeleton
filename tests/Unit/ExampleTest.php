7578b0398be02df09314c8e776245df2279854c1
Lê Chí Huy <lechihuy1062001@gmail.com>
2021-11-27 17:06:09 +0700

    test: unit test

---
+++ tests/Unit/ExampleTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Bit\Skeleton\Tests\Cli;
+
+use Bit\Skeleton\Tests\TestCase;
+
+class ExampleTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function test_example()
+    {
+        $this->assertTrue(true);
+    }
+}
