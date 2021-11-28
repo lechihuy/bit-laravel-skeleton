2a425bcded473205a955b1a9e29a0add29eafbab
Lê Chí Huy <lechihuy1062001@gmail.com>
2021-11-28 17:19:20 +0700

    refactor: entity

--- src/Entities/Entity.php
+++ src/Entities/Entity.php
@@ -2,7 +2,18 @@
 
 namespace Bit\Skeleton\Entities;
 
-abstract class Entity
-{
+use Illuminate\Contracts\Support\Arrayable;
 
+abstract class Entity implements Arrayable
+{
+    /**
+     * Create a new entity.
+     * 
+     * @param  mixed  ...$args
+     * @return void
+     */
+    public static function make(...$args)
+    {
+        return new static(...$args);
+    }
 }
