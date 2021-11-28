03e7d343061b0dc814ef23d74c5974bd16e808ce
Lê Chí Huy <lechihuy1062001@gmail.com>
2021-11-28 17:19:36 +0700

    feat: feature make command

---
+++ src/Generators/FeatureGenerator.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Bit\Skeleton\Generators;
+
+use Exception;
+use Bit\Skeleton\Generators\Generator;
+
+class FeatureGenerator extends Generator
+{
+    /**
+     * The service that owns the feature.
+     * 
+     * @var string
+     */
+    public $service;
+
+    /**
+     * Execute the generate command.   
+     * 
+     * @param  string  $name
+     * @param  string  $service
+     * @return void
+     */
+    public function generate(string $name, string $service): void
+    {
+        $this->name = $name;
+        $this->service = $service;
+        $this->basepath = service_path($service, 'Features');
+
+        if (is_null($service))
+            throw new InvalidArgumentException('Service option is required!');
+
+        if (Service::doesntHave($service))
+            throw new EntityNotFoundException("Service [{$service}] does not exist!");
+
+        if (Feature::has($name, $service))
+            throw new EntityAlreadyExistsException("Feature [{$service}] already exists!");
+
+        $this->makeFeatureFile();
+    }
+
+    /**
+     * Make the feature class file.
+     * 
+     * @return void
+     */
+    protected function makeFeatureFile()
+    {
+        $this->files->ensureDirectoryExists($this->basepath());
+
+        $this->files->put(
+            $this->basepath($this->name.'.php'),
+            $this->getstub('services/feature.stub', [
+                'service' => $this->service,
+                'feature' => $this->name,
+            ])
+        );
+    }
+}
