7d62b08b853269bfdcdf68b2423c919d06cbb462
Lê Chí Huy <lechihuy1062001@gmail.com>
2021-12-01 11:06:48 +0700

    feat: job & domain entity

---
+++ src/Entities/Job.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Bit\Skeleton\Entities;
+
+use Bit\Skeleton\Entities\Entity;
+
+class Job extends Entity
+{
+    /**
+     * The domain that owns the job.
+     * 
+     * @var string
+     */
+    public $domain;
+}
