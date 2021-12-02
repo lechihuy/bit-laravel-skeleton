887f095cde3774b3edc1b495baf9e2a7048a5bf4
Lê Chí Huy <lechihuy1062001@gmail.com>
2021-12-02 14:59:38 +0700

    feat: run a job in feature

--- src/Concepts/Feature.php
+++ src/Concepts/Feature.php
@@ -2,7 +2,19 @@
 
 namespace Bit\Skeleton\Concepts;
 
+use Illuminate\Support\Facades\App;
+
 abstract class Feature
 {
-    //
+    /**
+     * Run a job.
+     * 
+     * @param  string  $job
+     * @param  mixed  $args,...
+     * @return void
+     */
+    public function run($job, ...$args)
+    {
+        return App::call([new $job(...$args), 'handle']);
+    }
 }
